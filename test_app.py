1545b491ed1153986c8fdd4e4ff95cbef72c3bc1
Neha G M <nehagm77177@gmail.com>
2025-09-29 03:58:42 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 from app import app
 
+
 def test_hello():
     client = app.test_client()
     response = client.get('/')
-    assert response.data == b"Hello DevSecOps!"
+    assert response.data == b"Hello from dev!"
